5fb8c9a43a24fe0c48f4671bb30f4ec73f8fb261
autumnly1007 <autumnly1007@gmail.com>
2023-01-19 13:53:08 +0900

    Add search page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import dayjs from 'dayjs'
-import relativeTime from "dayjs/plugin/relativeTime";
-import "dayjs/locale/ko";
+import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
 
 //발행날짜 라이브러리
 dayjs.extend(relativeTime);
-dayjs.locale("ko");
+dayjs.locale('ko');
 
-const VideoCard = ({video}) => {
-// console.log('video', video)
+const VideoCard = ({ video }) => {
+  // console.log('video', video)
 
-const {channelTitle, publishedAt, thumbnails, title} = video.snippet
+  const { channelTitle, publishedAt, thumbnails, title } = video.snippet;
   return (
     <li className='cursor-pointer grid gap-4'>
-      <img src={thumbnails.standard.url}/>
+      <img src={thumbnails.standard ? thumbnails.standard.url : thumbnails.high.url} />
       <div>
-        <img/>
+        <img />
         <div className='grid gap-2'>
           <p className='line-clamp-3 leading-6'>{title}</p>
           <p>{channelTitle}</p>
@@ -23,7 +23,7 @@ const {channelTitle, publishedAt, thumbnails, title} = video.snippet
         </div>
       </div>
     </li>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
