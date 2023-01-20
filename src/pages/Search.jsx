74d30436653fbc0d6d608d0098a7c063dcebe7a7
autumnly1007 <autumnly1007@gmail.com>
2023-01-20 19:07:08 +0900

    Add search page infinity scroll

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,29 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { search } from '../api/request';
+import { getSearchVideos } from '../api/request';
 import VideoCard from '../components/VideoCard';
 
 const Search = () => {
-  const [videos, setVideos] = useState();
-
-  const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
-  };
-
-  let query = useQuery();
-  const searchTerm = query.get('query');
+  const [videos, setVideos] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState('');
+  const scrollRef = useRef();
+  const query = new URLSearchParams(useLocation().search).get('query');
 
   useEffect(() => {
-    if (!searchTerm) return;
-    search(searchTerm).then((data) => {
-      setVideos(data);
+    window.scrollTo(0, 0);
+    setVideos([]);
+    setNextPageToken('');
+    getSearchVideos(query).then((data) => {
+      setVideos(data.items);
+      setNextPageToken(data.nextPageToken);
     });
-  }, [searchTerm]);
+    infinityScroll();
+  }, [query]);
+
+  const infinityScroll = () => {
+    const option = {
+      threshold: 1,
+    };
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          getSearchVideos(query, nextPageToken).then((data) => {
+            setVideos((prev) => [...prev, ...data.items]);
+            setNextPageToken(data.nextPageToken);
+          });
+          observer.observe(scrollRef.current);
+        }
+      });
+    };
+    const observer = new IntersectionObserver(callback, option);
+    observer.observe(scrollRef.current);
+  };
 
   return (
     <ul className='w-full mt-16 p-4 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-10'>
       {videos &&
         videos.map((video) => <VideoCard key={video.id.videoId} video={video} type='search' />)}
+      <div className='w-1 h-5' ref={scrollRef}></div>
     </ul>
   );
 };
